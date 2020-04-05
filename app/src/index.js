b31a7adad08b8f11f5c951858a7f4ae74fdcde29
initiuminc1 <initiuminc1@gmail.com>
2020-04-05 10:59:50 +0530

    sending docker data with filtering

--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (_, res) => {
+	console.log("A new request received at " + Date.now());
     res.status(200).json({
         type: 'test'
     })
